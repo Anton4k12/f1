eead5ccb44105476a4188111a988e3fda94edd14
Anton4k12 <neanton2008@gmail.com>
2024-12-18 22:28:07 +0200

    fixes

--- src/components/leaderboard.tsx
+++ src/components/leaderboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { Interval } from "@/types";
 
